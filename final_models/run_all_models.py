5586e4c97e2cc591ab1974eb53e37ae130b25e81
patriciacarla04 <113781516+patriciacarla04@users.noreply.github.com>
2024-04-13 22:42:12 +0200

    Added data folder

--- final_models/run_all_models.py
+++ final_models/run_all_models.py
@@ -3,10 +3,10 @@ import subprocess
 
 # Define the command for each model training
 commands = [
-    ['python', 'xlmr_multilingual.py', '--train_data', 'multi_duplicate_all.csv', '--test_data_en', 'EN_test.csv', '--test_data_it', 'IT_test.csv', '--test_data_slo','SLO_test.csv', '--output_dir', 'xlmr_duplicate_all', '--results_dir', 'results_duplicate_all', '--batch_size', '32', '--max_len','128','--epochs','1'],
-    ['python', 'xlmr_multilingual.py', '--train_data', 'multi_duplicate_disagreement.csv', '--test_data_en', 'EN_test.csv', '--test_data_it', 'IT_test.csv', '--test_data_slo','SLO_test.csv', '--output_dir', 'xlmr_duplicate_disagreement', '--results_dir', 'results_duplicate_disagreement', '--batch_size', '32', '--max_len','128','--epochs','1'],
-    ['python', 'xlmr_multilingual.py', '--train_data', 'multi_remove_disagreement.csv', '--test_data_en', 'EN_test.csv', '--test_data_it', 'IT_test.csv', '--test_data_slo','SLO_test.csv', '--output_dir', 'xlmr_remove_disagreement', '--results_dir', 'results_remove_disagreement', '--batch_size', '32', '--max_len','128','--epochs','1'],
-    ['python', 'xlmr_disagreement_class.py', '--train_data', 'multi_disagreement_class.csv', '--test_data', 'multi_disagreement_class_test_test.csv', '--output_dir', 'xlmr_disagreement_class', '--results_dir', 'results_disagreement_class', '--batch_size', '32', '--max_len','128','--epochs','1']
+    ['python', 'xlmr_multilingual.py', '--train_data', 'final_models/data/multi_duplicate_all.csv', '--test_data_en', 'final_models/data/EN_test.csv', '--test_data_it', 'final_models/data/IT_test.csv', '--test_data_slo','final_models/data/SLO_test.csv', '--output_dir', 'xlmr_duplicate_all', '--results_dir', 'results_duplicate_all', '--batch_size', '32', '--max_len','128','--epochs','1'],
+    ['python', 'xlmr_multilingual.py', '--train_data', 'final_models/data/multi_duplicate_disagreement.csv', '--test_data_en', 'final_models/data/EN_test.csv', '--test_data_it', 'final_models/data/IT_test.csv', '--test_data_slo','final_models/data/SLO_test.csv', '--output_dir', 'xlmr_duplicate_disagreement', '--results_dir', 'results_duplicate_disagreement', '--batch_size', '32', '--max_len','128','--epochs','1'],
+    ['python', 'xlmr_multilingual.py', '--train_data', 'final_models/data/multi_remove_disagreement.csv', '--test_data_en', 'final_models/data/EN_test.csv', '--test_data_it', 'final_models/data/IT_test.csv', '--test_data_slo','final_models/data/SLO_test.csv', '--output_dir', 'xlmr_remove_disagreement', '--results_dir', 'results_remove_disagreement', '--batch_size', '32', '--max_len','128','--epochs','1'],
+    ['python', 'xlmr_disagreement_class.py', '--train_data', 'final_models/data/multi_disagreement_class.csv', '--test_data', 'final_models/data/multi_disagreement_class_test_test.csv', '--output_dir', 'xlmr_disagreement_class', '--results_dir', 'results_disagreement_class', '--batch_size', '32', '--max_len','128','--epochs','1']
 ]
 
 # List to hold the processes
